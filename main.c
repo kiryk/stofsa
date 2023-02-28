cac67bf588873ca723d185fb39a63151d75694bb
kiryk <alkiryk@wp.pl>
2023-02-28 22:56:40 +0100

    implement full ascii version

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 struct state;
-struct transition;
+struct trans;
 
 struct trans {
 	struct trans *next;
@@ -22,6 +22,8 @@ int cmp_state(struct state *key, struct state *dat)
 
 	int diff;
 
+  if ((diff = dat->accepts - key->accepts) != 0)
+		return diff;
 	while (ktr && dtr) {
 		if ((diff = ktr->rune - dtr->rune) != 0)
 			return diff;
@@ -39,7 +41,7 @@ int cmp_state(struct state *key, struct state *dat)
 
 void add_trans(struct state *src, struct state *dst, int rune)
 {
-	struct trans *tr = malloc(sizeof(*tr));
+	struct trans *tr = calloc(1, sizeof(*tr));
 
 	tr->next = src->trans;
 	src->trans = tr;
@@ -49,20 +51,74 @@ void add_trans(struct state *src, struct state *dst, int rune)
 
 void free_state(struct state *st)
 {
-	struct trans *tr = st->trans;
+	struct trans *tr, *next;
 
-	for (; tr; tr = tr->next) {
+	for (tr = st->trans; tr; tr = next) {
+		next = tr->next;
 		free_state(tr->state);
 		free(tr);
 	}
 	free(st);
 }
 
+void add_string(struct state *st, char *s)
+{
+	struct state *nst;
+	int i;
+
+	for (i = 0; s[i]; st = nst, i++) {
+		nst = calloc(1, sizeof(*nst));
+		add_trans(st, nst, s[i]);
+	}
+	st->accepts = 1;
+}
+
+struct state *get_last(struct state *st, char *s, int *last)
+{
+	struct trans *tr = st->trans;
+	int i;
+
+	for (i = 0; s[i]; i++) {
+		if (!st->trans || st->trans->rune != s[i])
+			break;
+		st = st->trans->state;
+	}
+	*last = i;
+	return st;
+}
+
+void unify_state(struct state *uniq, struct state *st)
+{
+	struct state *last = st->trans->state;
+	struct trans *tr;
+
+	if (last->trans)
+		unify_state(uniq, last);
+	for (tr = uniq->trans; tr; tr = tr->next)
+		if (cmp_state(last, tr->state) == 0)
+			break;
+	if (tr) {
+		free_state(st->trans->state);
+		st->trans->state = tr->state;
+	} else {
+		add_trans(uniq, last, 0);
+	}
+}
+
 int main()
 {
-	struct state unique; /* points to unique states */
+	struct state uniq = {}; /* points to unique states */
+	struct state fsa = {};  /* the FSA being built */
+	struct state *last;
+	int length;
 	char word[50];
-	while (scanf("%s", word) >= 0)
-		;
+
+	while (scanf("%s", word) >= 0) {
+		last = get_last(&fsa, word, &length);
+		if (last->trans)
+			unify_state(&uniq, last);
+		add_string(last, word+length);
+	}
+	unify_state(&uniq, &fsa);
 	exit(0);
 }
